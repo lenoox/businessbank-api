656d0deedbf54de60ea20ff4a7aa607a76b853c4
lenoox <62141973+lenoox@users.noreply.github.com>
2023-04-07 23:27:10 +0200

    init projekt

---
+++ src/main/java/com/lenoox/businessbank/BusinessbankApplication.java
@@ -0,0 +1,13 @@
+package com.lenoox.businessbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lenoox/businessbank/BusinessbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenoox.businessbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
